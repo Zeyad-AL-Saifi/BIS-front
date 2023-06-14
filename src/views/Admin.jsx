64def166402a3096ae1f36d688212e13b785ee42
ziadalsafe <zalsafe4@gmail.com>
2023-06-14 16:53:23 +0300

    we

--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import SideNav from "../components/admin-components/SideNav";
 import HomeManagement from "./admin-views/HomeManagement";
 import TeacherManagement from "./admin-views/TeacherManagement";
+import { Route ,BrowserRouter as Router,Routes } from "react-router-dom";
+import StudentManagement from "./admin-views/StudentManagement";
 
 const Admin = () => {
   return (
-    <div className="row">
-      <div className="col-auto">
-        <SideNav />
+         <div className="row">
+        <div className="col-auto">
+          <SideNav />
+        </div>
+        <div className="col col-md-7 col-lg-8  col-xl-9 col-sm-9 ">
+          <Route path="/teachermanagement" component={<TeacherManagement />} />
+          <Route path="/homecontentmanagment" component={<HomeManagement/>} />
+          <Route path="/studentmanagement" component={<StudentManagement/>} />
+
+        </div>
       </div>
-      <div className="col col-md-7 col-lg-8  col-xl-9 col-sm-9 ">
-        {/* <HomeManagement /> */}
-        <TeacherManagement />
-      </div>
-    </div>
+     
+
+    
+
   );
 };
 
